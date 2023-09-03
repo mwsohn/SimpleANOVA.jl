863afe035c06140c5c7f68f056eebe9508d6e051
mwsohn <mwsohn@gmail.com>
2023-09-03 16:56:44 -0400

    Update anova_dataframes.jl

--- src/anova_dataframes.jl
+++ src/anova_dataframes.jl
@@ -2,9 +2,9 @@ using .DataFrames
 
 function anova(df::DataFrame, observationscolumn::Symbol, factorcolumns::Vector{Symbol}, factortypes::Vector{FactorType} = FactorType[]; factornames::Vector{<:AbstractString} = String[])
     df2 = df[completecases(df,vcat(observationscolumn, factorcolumns)),:]
-    observations = df2[!, observationscolumn]
+    observations = convert(Vector{Float64},df2[!, observationscolumn])
     length(observations) > 0 || return
-    nonmissingtype(eltype(observations)) <: Number || error("Obervations must be numeric")
+    eltype(observations) <: Number || error("Obervations must be numeric")
     isempty(factornames) && (factornames = [String(col) for col ∈ factorcolumns])
     anova(observations, [df2[!, x] for x ∈ factorcolumns], factortypes, factornames = factornames)
 end
